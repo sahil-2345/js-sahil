6d901dd13057b4ae9add5572be141a6b1ee1c450
sahil-2345 <151909842+sahil-2345@users.noreply.github.com>
2024-09-22 08:38:07 +0000

    code save

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,16 @@
+const gameName = new String('sahil-bsc')
+
+// console.log(gameName[1]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.toLowerCase());
+// console.log(gameName.charAt(4));
+// console.log(gameName.indexOf('c'));
+
+const newString = gameName.substring(0, 4)
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 4)
+console.log(anotherString);
---
+++ 01_basics/06_numsandmaths.js
@@ -0,0 +1,34 @@
+const balance = new Number(200)
+// console.log(balance);
+
+const score = 400
+// console.log(score);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+const otherNumber = 234.8966
+// console.log(otherNumber.toPrecision(5));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-TR'));
+
+////////////////  Maths ////////////////////
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(7.6));
+// console.log(Math.ceil(7.2));
+// console.log(Math.floor(7.9));
+
+console.log(Math.random());
+console.log(Math.random()*10);
+console.log((Math.random()*10) + 1);
+console.log((Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
+
+
